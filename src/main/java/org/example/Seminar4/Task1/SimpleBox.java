c4ba473d7f5b24ff202366443b5d8ee3b77cd416
Guillaumette <anna_kniazeva@bk.ru>
2023-07-10 15:51:27 +0300

    sem4 task1 meeting with generics

---
+++ src/main/java/org/example/Seminar4/Task1/SimpleBox.java
@@ -0,0 +1,12 @@
+package org.example.Seminar4.Task1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class SimpleBox {
+    private Object obj;
+}
