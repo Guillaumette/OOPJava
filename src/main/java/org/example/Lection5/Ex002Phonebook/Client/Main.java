3a15f434f82d5a6f67bef509ce4328b08b588433
Guillaumette <anna_kniazeva@bk.ru>
2023-07-13 14:11:41 +0300

    lect5 phonebook creation

---
+++ src/main/java/org/example/Lection5/Ex002Phonebook/Client/Main.java
@@ -0,0 +1,9 @@
+package org.example.Lection5.Ex002Phonebook.Client;
+
+import org.example.Lection5.Ex002Phonebook.UI.App;
+
+public class Main {
+    public static void main(String[] args) {
+        App.ButtonClick();
+    }
+}
