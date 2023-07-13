6a5cb158b4aae5a194052eed10fca2ad1e4f46b6
Guillaumette <anna_kniazeva@bk.ru>
2023-07-13 13:09:50 +0300

    lect5 sum of two numbers with model, view, presenter

---
+++ src/main/java/org/example/Lection5/Ex001Calc/CalcModel.java
@@ -0,0 +1,5 @@
+package org.example.Lection5.Ex001Calc;
+
+public abstract class CalcModel implements Model{
+    int x, y;
+}
---
+++ src/main/java/org/example/Lection5/Ex001Calc/Main.java
@@ -0,0 +1,8 @@
+package org.example.Lection5.Ex001Calc;
+
+public class Main {
+    public static void main(String[] args) {
+        Presenter p = new Presenter(new SumModel(), new View());
+        p.buttonClick();
+    }
+}
