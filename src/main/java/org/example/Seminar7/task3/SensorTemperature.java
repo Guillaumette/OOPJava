81210dd2745c3a47640a652cd6603649fce8bb07
Guillaumette <anna_kniazeva@bk.ru>
2023-07-20 15:37:09 +0300

    sem7 pattern adapter

---
+++ src/main/java/org/example/Seminar7/task3/SensorTemperature.java
@@ -0,0 +1,10 @@
+package org.example.Seminar7.task3;
+
+// Интерфейс дополнительного датчика
+interface SensorTemperature{
+    int identifier(); // идентификатор датчика
+    double temperature(); // температура датчика
+    int year();			 // Год
+    int day();			 // День года
+    int second();		 // Секунда дня
+}
